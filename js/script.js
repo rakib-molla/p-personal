06b85252814def9306f57555b27b99f7824a25c0
rakib-molla <rakibmail1997@gmail.com>
2024-02-24 01:53:52 +0600

    image mouse enter effect add

--- js/script.js
+++ js/script.js
@@ -30,29 +30,29 @@ function firstPageAnim() {
 
 var timeOut;
 
-function circleSlimWhenMouseMove(){
-   // define default scale value
+function circleSlimWhenMouseMove() {
+  // define default scale value
 
-   var xprev = 0;
-   var yprev = 0;
+  var xprev = 0;
+  var yprev = 0;
 
-   window.addEventListener("mousemove", function(details){
-     clearTimeout(timeOut)
-      xscale =   gsap.utils.clamp(.8, 1.2, details.clientX - xprev);
-      yscale =   gsap.utils.clamp(.8, 1.2, details.clientY - yprev);
+  window.addEventListener("mousemove", function (details) {
+    clearTimeout(timeOut);
+    xscale = gsap.utils.clamp(0.8, 1.2, details.clientX - xprev);
+    yscale = gsap.utils.clamp(0.8, 1.2, details.clientY - yprev);
 
-      xprev = details.clientX;
-      yprev = details.clientY;
+    xprev = details.clientX;
+    yprev = details.clientY;
 
-      circleMouseFollower(xscale, yscale);
+    circleMouseFollower(xscale, yscale);
 
-     timeOut = this.setTimeout(function(){
-      document.querySelector('#mini-circle').style.transform = `translate(${details.clientX}px, ${details.clientY}px) scale(1, 1)`
-     });
-
-   })
+    timeOut = this.setTimeout(function () {
+      document.querySelector(
+        "#mini-circle"
+      ).style.transform = `translate(${details.clientX}px, ${details.clientY}px) scale(1, 1)`;
+    });
+  });
 }
-
 
 function circleMouseFollower(xscale, yscale) {
   window.addEventListener("mousemove", function (details) {
@@ -65,4 +65,38 @@ function circleMouseFollower(xscale, yscale) {
 
 circleMouseFollower();
 firstPageAnim();
-circleSlimWhenMouseMove()
+circleSlimWhenMouseMove();
+
+// when mouse enter image section then select three element and apply each element
+
+document.querySelectorAll(".element").forEach(function (singleElement) {
+
+   var rotate = 0;
+   var diffRot = 0;
+
+  singleElement.addEventListener("mousemove", function (details) {
+   
+   var diff = details.clientY - singleElement.getBoundingClientRect().top;
+   var diffRot = details.clientX - rotate;
+   rotate = details.clientX
+    gsap.to(singleElement.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff,
+      left: details.clientX,
+      rotate: gsap.utils.clamp(-20,20, diffRot * 0.5)
+    });
+  });
+});
+
+// mouse leave then hide image
+document.querySelectorAll(".element").forEach(function (singleElement) {
+   singleElement.addEventListener("mouseleave", function (details) {
+    
+     gsap.to(singleElement.querySelector("img"), {
+       opacity: 0,
+       ease: Power3,
+       duration: 0.5,
+     });
+   });
+ });
