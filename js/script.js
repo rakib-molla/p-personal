53180ab911de7ec06168109a96c109772bc39102
rakib-molla <rakibmail1997@gmail.com>
2024-02-23 11:34:33 +0600

    hero section animation add

--- js/script.js
+++ js/script.js
@@ -3,11 +3,29 @@ const scroll = new LocomotiveScroll({
    smooth: true
 });
 
+function firstPageAnim(){
+   var tl = gsap.timeline();
+
+   tl.from('#nav',{
+      y: '-10',
+      opacity: 0,
+      duration: 2,
+      ease: Expo.easeInOut
+   })
+   .to('.boundingelem',{
+      y:0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      stagger: .2,
+   })
+}
+
 function circleMouseFollower(){
   window.addEventListener("mousemove", function(details){
-   console.log(details.clientX, details.clientY)
+   // console.log(details.clientX, details.clientY)
    document.querySelector("#mini-circle").style.transform = `translate(${details.clientX}px, ${details.clientY}px)`
   })
 }
 
 circleMouseFollower();
+firstPageAnim()
